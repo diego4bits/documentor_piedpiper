b35c42e02833c587e696e18931311585c111ce1e
Diego Benavides <diego4bits@proton.me>
2023-09-11 16:41:34 -0500

    ahi vamos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from documentation_orchestrator.documentation_manager import DocumentationManager
 #from doc_writer.doc_writer import DocWriter
-
+from git_tools.git_file_paths import GitFilePaths
 if __name__ == "__main__":
-    DocumentationManager.start_documentation()
-    #DocWriter.start_writing_documentation()
-    DocumentationManager.show_results()
+    # DocumentationManager.start_documentation()
+    # #DocWriter.start_writing_documentation()
+    # DocumentationManager.show_results()
+    print(GitFilePaths.get_all_valid_paths())
