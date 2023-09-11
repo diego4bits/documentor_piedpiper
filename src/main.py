77c0aa59382ef73de160d75c2adeacba394d4c76
Diego Benavides <diego4bits@proton.me>
2023-09-11 16:56:19 -0500

    change in code scrapper extract snippets and main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from documentation_orchestrator.documentation_manager import DocumentationManager
-#from doc_writer.doc_writer import DocWriter
-from git_tools.git_file_paths import GitFilePaths
+from doc_writer.doc_writer import DocWriter
+
 if __name__ == "__main__":
     DocumentationManager.start_documentation()
-    #DocWriter.start_writing_documentation()
+    DocWriter.start_writing_documentation()
     DocumentationManager.show_results()
