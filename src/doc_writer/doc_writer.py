4f935bc19907f9a49c6edd0db2c75c00349b8f91
Diego Benavides <diego4bits@proton.me>
2023-09-11 21:42:58 -0500

    doc writer changed docpath to ./docs_website/docs

--- src/doc_writer/doc_writer.py
+++ src/doc_writer/doc_writer.py
@@ -8,7 +8,7 @@ from metaclasses.singleton_meta import SingletonMeta
 
 class DocWriter(metaclass=SingletonMeta):
     instance: ClassVar
-    doc_path: Path = Path("./docs")
+    doc_path: Path = Path("./docs_website/docs")
     snippets_to_doc: Dict[int, CodeSnippet] = None
     current_snippet: CodeSnippet = None
     current_doc_path: Path = None
