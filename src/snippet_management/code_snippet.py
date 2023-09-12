492da09d3f1e2e741665494c7dc03ce52218234c
Diego Benavides <diego4bits@proton.me>
2023-09-11 22:10:38 -0500

    correcion typo en yml carpeta docs_template

--- src/snippet_management/code_snippet.py
+++ src/snippet_management/code_snippet.py
@@ -15,7 +15,7 @@ class CodeSnippet:
     parent_identifier: str
     code_snippet_type: str
     code_snippet_identifier: str
-    code_snippet_implementation: str = field(hash=False) #ccambio
+    code_snippet_implementation: str = field(hash=False) 
 
     def __init__(
         self,
